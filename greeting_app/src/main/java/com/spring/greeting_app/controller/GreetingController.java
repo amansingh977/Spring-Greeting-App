3d46ffc69cd41d2aabebb340a59325bc140706fd
amansingh977 <amansinghrajawat09@gmail.com>
2025-02-22 13:30:45 +0530

    Aman Add. uc1 - Return JSON responses for different HTTP methods

---
+++ greeting_app/src/main/java/com/spring/greeting_app/controller/GreetingController.java
@@ -0,0 +1,35 @@
+package com.spring.greeting_app.controller;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
+
+// Marks this class as a REST Controller that returns JSON responses
+@RestController
+@RequestMapping("/greet")
+public class GreetingController {
+
+    // uc1 - Handles HTTP GET request and returns a greeting message as JSON
+    @GetMapping
+    public Map<String, String> getGreeting() {
+        return Map.of("message", "Hello, welcome to our Spring Boot app!");
+    }
+
+    // uc1 - Handles HTTP POST request and returns a creation message as JSON
+    @PostMapping
+    public Map<String, String> postGreeting() {
+        return Map.of("message", "Greeting has been created!");
+    }
+
+    // uc1 - Handles HTTP PUT request and returns an update message as JSON
+    @PutMapping
+    public Map<String, String> putGreeting() {
+        return Map.of("message", "Greeting has been updated!");
+    }
+
+    // uc1 - Handles HTTP DELETE request and returns a deletion message as JSON
+    @DeleteMapping
+    public Map<String, String> deleteGreeting() {
+        return Map.of("message", "Greeting has been deleted!");
+    }
+}
+
